f7248b198178a1af034e32d4d3e669fe8761f0a6
Viðar Ingi Ottósson <viddi@nplexity.com>
2014-09-11 00:14:06 -0400

    Progress on update script

--- update.py
+++ update.py
@@ -3,11 +3,14 @@
 import os
 import sys
 
-os.system('cd ..')
 os.system('wget https://github.com/Viddi/Mpi/archive/master.zip')
 os.system('unzip master.zip')
-os.system('rm -rf Mpi/src')
-os.system('mkdir Mpi/src')
-os.system('cp Mpi-Master/src/* Mpi/src/')
+os.system('rm -rf Mpi-Master/lib/mpich')
+os.system('cp -rf Mpi/lib/* Mpi-Master/lib')
+os.system('rm -rf Mpi')
+os.system('mv Mpi-Master Mpi')
+
+# Clean up
+os.system('rm -rf master.zip')
 
 # TOOD: Update library files (exclude binaries)
