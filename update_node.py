382b64a91aeee216ee6b049640b0f25d3ad7333b
Viðar Ingi Ottósson <viddi@nplexity.com>
2014-10-22 10:49:20 -0400

    Updated script

--- update_node.py
+++ update_node.py
@@ -9,5 +9,8 @@ f.close
 
 for node in nodes:
     if node != '10.10.1.236'
+        os.system('ssh pi@' + node + ' mv ~/Development/Mpi/lib ~/Development/')
         os.system('ssh pi@' + node + ' rm -rf ~/Development/Mpi')
         os.system('scp -r Mpi/ pi@' + node + ':/home/pi/Development')
+        os.system('ssh pi@' + node + ' rm -rf ~/Development/Mpi/lib')
+        os.system('ssh pi@' + node + ' mv ~/Development/lib ~/Development/Mpi')
