112efc5ac9aada273c58f474ebd80b0e243a7749
Viðar Ingi Ottósson <viddi@nplexity.com>
2014-10-24 10:27:47 -0400

    Fixed update script

--- update.py
+++ update.py
@@ -3,14 +3,14 @@
 import os
 import sys
 
-os.system('wget -O latest.zip https://github.com/Viddi/Mpi/archive/master.zip')
+os.system('wget -O latest.zip https://github.com/Viddi/Mpi/archive/latest.zip')
 
 if os.path.exists('latest.zip'):
-    os.system('unzip master.zip')
+    os.system('unzip latest.zip')
     os.system('rm -rf Mpi-master/lib/mpich')
     os.system('cp -rf Mpi/lib/* Mpi-master/lib')
     os.system('rm -rf Mpi')
     os.system('mv Mpi-master Mpi')
     
     # Clean up
-    os.system('rm -rf master.zip')
+    os.system('rm -rf latest.zip')
